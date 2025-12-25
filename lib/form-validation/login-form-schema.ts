6803819ef170a259358ced46e62994c73122a5ef
Mohamed Safras <awsafras@gmail.com>
2025-12-25 20:27:42 +0530

    fix:login input validation

--- lib/form-validation/login-form-schema.ts
+++ lib/form-validation/login-form-schema.ts
@@ -1,18 +1,13 @@
 // /types/sign-up-form-schema.ts
 import { z } from "zod";
-import { passwordPolicy } from "../common/form-validation-policy";
-
-// Baseline password policy: 8+ chars, 1 upper, 1 lower, 1 number, 1 special.
 
 export const LoginFormScheama = z.object({
-  email: z.string().trim().toLowerCase().email("Enter a valid email address"),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .regex(
-      passwordPolicy,
-      "Use upper & lower case letters, a number, and a special character"
-    ),
+  email: z
+    .string({ required_error: "Email is required" })
+    .trim()
+    .toLowerCase()
+    .email("Enter a valid email address"),
+  password: z.string({ required_error: "Password is required" }),
 });
 
 export type SignUpForm = z.infer<typeof LoginFormScheama>;
